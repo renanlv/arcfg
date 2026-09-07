cfd3ed9c4c5efea975c63c805987095e0ea573fa
renanlv <piresrenansilv@gmail.com>
2026-09-07 18:55:48 -0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -175,7 +175,7 @@ install_microcode_drivers() {
 }
 
 install_base_packages() {
-    sudo pacman -S --noconfirm git 7zip aria2 gstreamer ffmpeg flatpak tealdeer fastfetch msedit gamemode fwupd arch-update
+    sudo pacman -S --noconfirm git 7zip aria2 ffmpeg flatpak tealdeer fastfetch msedit gamemode fwupd arch-update
 }
 
 install_desktop() {
