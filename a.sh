a6fbafabf9e0d9a841badb662d4a74a8795842a4
renanlv <piresrenansilv@gmail.com>
2026-09-09 07:07:45 -0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -175,7 +175,7 @@ install_microcode_drivers() {
 }
 
 install_base_packages() {
-    sudo pacman -S --noconfirm 7zip gamemode fwupd flatpak arch-update
+    sudo pacman -S --noconfirm gamemode fwupd flatpak fastfetch arch-update
 }
 
 install_desktop() {
