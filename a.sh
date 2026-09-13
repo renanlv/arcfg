0ad225892385227bae07aaf797571c15735fde4a
renanlv <piresrenansilv@gmail.com>
2026-09-13 13:15:50 -0300

    Update a.sh

--- a.sh
+++ a.sh
@@ -68,9 +68,7 @@ fi
 echo -e "${GREEN}Sistema atualizado com sucesso, pressione enter para sair${NC}"
 read -r
 EOF
-
     sudo chmod +x /usr/local/bin/system-update
-
     sudo tee /usr/share/applications/system-update.desktop > /dev/null <<'EOF'
 [Desktop Entry]
 Type=Application
